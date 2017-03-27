5944c277b55f2b9a158c4a3a6766c908720a8a16
Erik Lott <mreriklott@gmail.com>
2017-03-27 17:02:03 -0400

    documentation

--- doc.go
+++ doc.go
@@ -3,65 +3,27 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package mustache is a full implementation of the mustache template language. For
-more information on the mustache spec, refer to the mustache manual at
-https://mustache.github.io/mustache.5.html
-
-Mustache Spec Compliance
-This mustache implementation complies with 100% of the mustache specs, including
-lambda specs.
+Package mustache is a full implementation of the mustache template language,
+complying with 100% of the mustache specs - including lambda specs. For
+more information on the mustache specs, visit https://github.com/mustache/spec
 
 Documentation
 
+To learn how to use the mustache language, visit the mustache guide at
+https://mustache.github.io/mustache.5.html
 
 
-Templates And Partials
+Installation
 
-Templates and partials are both considered 'partials' by this library. Both
-must be named, and are parsed and rendered in the same fashion.
+To install, run:
+        go get -u github.com/eriklott/mustache
 
-Basic Template
-        tmpl := mustache.NewTemplate()
-        tmpl.ParseString("main", `Hello {{word}}`)
-        tmpl.Render(os.Stdout, "main", map[string]string{"word": "World!"})
-        // prints: "Hello World!"
+Import into your project using:
+        import "github.com/eriklott/mustache"
 
-Template with Partial
-        tmpl := mustache.NewTemplate()
-        tmpl.ParseString("main", `Hello {{>mypartial}}`)
-        tmpl.ParseString("mypartial", `World!`)
-        tmpl.Render(os.Stdout, "main", nil)
-        // prints: "Hello World!"
+Usage
 
-Lambdas
-
-A lambda can be used as the context data for variable or section tags.
-
-When used as the data for a variable tag, the lambda must be an arity 0 function
-that returns a string.
-        lambda := func() string {
-                return "Hello World!"
-        }
-
-        tmpl := mustache.NewTemplate()
-        tmpl.ParseString("main", `{{lambda}}`)
-        tmpl.Render(os.Stdout, "main", map[string]interface{}{"lambda": lambda})
-        // prints: "Hello World!"
-
-
-When used as the data for a section tag, the lambda must be an arity 1 function
-that accepts a string argument, and returns a string.
-        lambda := func(txt string) string {
-                return "Hello " + txt
-        }
-
-        tmpl := mustache.NewTemplate()
-        tmpl.ParseString("main", `{{#lambda}}World!{{/lambda}}`)
-        tmpl.Render(os.Stdout, "main", map[string]interface{}{"lambda": lambda})
-        // prints: "Hello World!"
-
-A function provided as data in any other form will be ignored and considered
-a 'falsey' value.
+See the examples below for usage:
 
 */
 package mustache
---
+++ doc_test.go
@@ -0,0 +1,59 @@
+package mustache
+
+import (
+	"os"
+
+	"github.com/eriklott/mustache"
+)
+
+func Example_basic() {
+	t := mustache.NewTemplate()
+	t.ParseString("main", `Hello {{word}}`)
+	t.Render(os.Stdout, "main", map[string]string{"word": "World!"})
+	// "Hello World!"
+}
+
+func Example_partial() {
+	t := mustache.NewTemplate()
+	t.ParseString("main", `Hello {{>mypartial}}`)
+	t.ParseString("mypartial", `World!`)
+	tmpl.Render(os.Stdout, "main", nil)
+	// "Hello World!"
+}
+
+// A lambda rendered by a variable tag must be a arity 0 function that returns
+// a string. Any function not conforming to this signature will be considered
+// falsey.
+func Example_lambda() {
+	lambda := func() string {
+		return "Hello World!"
+	}
+	t := mustache.NewTemplate()
+	t.ParseString("main", `{{lambda}}`)
+	t.Render(os.Stdout, "main", map[string]interface{}{"lambda": lambda})
+	// "Hello World!"
+}
+
+// A lambda rendered by a section tag must be a arity 1 function that accepts
+// a string argument, and returns a string. Any function not conforming to this
+// signature will be considered falsey by the section tag.
+func Example_lambdaSection() {
+	lambda := func(txt string) string {
+		return "Hello " + txt
+	}
+	t := mustache.NewTemplate()
+	t.ParseString("main", `{{#lambda}}World!{{/lambda}}`)
+	t.Render(os.Stdout, "main", map[string]interface{}{"lambda": lambda})
+	// "Hello World!"
+}
+
+// Dotted names should be considered a form of shorthand for sections.
+func Example_dottedNames() {
+	person := map[string]string{"name": "Joe"}
+	data := map[string]interface{}{"person": person}
+
+	t := mustache.NewTemplate()
+	t.ParseString("main", `{{person.name}}`)
+	t.Render(os.Stdout, "main", data)
+	// "Joe"
+}
